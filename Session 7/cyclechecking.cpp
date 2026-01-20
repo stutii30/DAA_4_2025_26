c8a5b48ed0124c7ae6fa8dfdbf2a1f378653d5f9
stutii30 <stutisharma3012@gmail.com>
2026-01-20 21:01:25 +0530

    Add files via upload

---
+++ Session 7/cyclechecking.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution
+{
+public:
+    ListNode *detectCycle(ListNode *head)
+    {
+        ListNode *slow = head;
+        ListNode *fast = head;
+        int pos = 0;
+        while (fast != NULL && fast->next != NULL)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+            if (slow == fast)
+            {
+                slow = head;
+                while (slow != fast)
+                {
+                    slow = slow->next;
+                    fast = fast->next;
+                    pos++;
+                }
+                cout << "tail connects to node index" << pos;
+                return slow;
+            }
+        }
+        cout << "no cycle";
+        return NULL;
+    }
+};
